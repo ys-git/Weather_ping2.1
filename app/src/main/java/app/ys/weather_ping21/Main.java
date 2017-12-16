057a2463f02898e29262b27054793b588ea878d1
YS <yogendrasingh890@gmail.com>
2017-12-16 11:57:15 +0530

    Added Alert Dialog for Location permission

--- app/src/main/java/app/ys/weather_ping21/Main.java
+++ app/src/main/java/app/ys/weather_ping21/Main.java
@@ -1,94 +1,183 @@
 package app.ys.weather_ping21;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
-import android.text.Html;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * Created by YS on 14-12-2017.
- */
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class Main extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
-    SharedPreferences sdata;
-    TextView t1,t2;
-    String s,q;
-    public Double lat,lon;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import java.net.URL;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    // Project Created by Ferdousur Rahman Shajib
+    // www.androstock.com
+
+
+    TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField;
+
+    Typeface weatherFont;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    double lat,lon;
+    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 1;
+    private GoogleApiClient googleApiClient;
     LocationManager locationManager;
-    String la,lo;
-    TextView city, details, temp, hum, pre, weatherIcon, updated;
+    String s,q;
+    //TextView t1,t2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.main);
-        //t1=(TextView)findViewById(R.id.textView3);
-        //t2=(TextView)findViewById(R.id.textView2);
+        getLocation();
 
-        city = (TextView)findViewById(R.id.tv);
-        t1 = (TextView)findViewById(R.id.textView8);
-        Intent intent = getIntent();
-        la = intent.getStringExtra("lat");
-        lo = intent.getStringExtra("lon");
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
 
+            //ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+            new AlertDialog.Builder(this)
+                    .setTitle("Location Permission")
+                    .setMessage("Please allow the app to access Device Location")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    //Prompt the user once explanation has been shown
+                                    ActivityCompat.requestPermissions(Main.this,
+                                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                            MY_PERMISSIONS_REQUEST_LOCATION);
+                                }
+        }
 
 
 
-        //getLocation();
-        //if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-           // ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
-
-
+       // if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        //        != PackageManager.PERMISSION_GRANTED) {
+        //    ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION },
+        //            PERMISSION_ACCESS_COARSE_LOCATION);
         //}
-        s=String.valueOf(la);
-        q=String.valueOf(lo);
-        t1.setText(s);
-
-        //cityField = (TextView)findViewById(R.id.city_field);
-        //updatedField = (TextView)findViewById(R.id.updated_field);
-        //detailsField = (TextView)findViewById(R.id.details_field);
-        //currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
-        //humidity_field = (TextView)findViewById(R.id.humidity_field);
-        //pressure_field = (TextView)findViewById(R.id.pressure_field);
-        ///weatherIcon = (TextView)findViewById(R.id.weather_icon);
-        //weatherIcon.setTypeface(weatherFont);
 
 
+        googleApiClient = new GoogleApiClient.Builder(this, this, this).addApi(LocationServices.API).build();
 
-        Fetch.placeIdTask asyncTask =new Fetch.placeIdTask(new Fetch.AsyncResponse() {
-            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
+    }
 
-                city.setText(weather_city);
-                //updatedField.setText(weather_updatedOn);
-                //detailsField.setText(weather_description);
-                //currentTemperatureField.setText(weather_temperature);
-                //humidity_field.setText("Humidity: "+weather_humidity);
-                //pressure_field.setText("Pressure: "+weather_pressure);
-                //weatherIcon.setText(Html.fromHtml(weather_iconText));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // All good!
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
 
-            }
-        });
-        asyncTask.execute("12.840","80.903"); //  asyncTask.execute("Latitude", "Longitude")
+                break;
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(Main.class.getSimpleName(), "Connected to Google Play Services!");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            //t1.setText("Latitude: " + lastLocation.getLatitude() + "\n Longitude: " + lastLocation.getLongitude());
+
+            lat = lastLocation.getLatitude();
+            lon = lastLocation.getLongitude();
+            //t1 = (TextView)findViewById(R.id.t);
+            //t2 = (TextView)findViewById(R.id.y);
+
+
+            s=String.valueOf(lat);
+            q=String.valueOf(lon);
+            //t1.setText(s);
+            //t2.setText(q);
+
+            weatherFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/weathericons-regular-webfont.ttf");
+
+            cityField = (TextView)findViewById(R.id.textView8);
+            //updatedField = (TextView)findViewById(R.id.updated_field);
+            //detailsField = (TextView)findViewById(R.id.details_field);
+            //currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
+            //humidity_field = (TextView)findViewById(R.id.humidity_field);
+            //pressure_field = (TextView)findViewById(R.id.pressure_field);
+            //weatherIcon = (TextView)findViewById(R.id.weather_icon);
+            //weatherIcon.setTypeface(weatherFont);
+
+
+
+            Fetch.placeIdTask asyncTask =new Fetch.placeIdTask(new Fetch.AsyncResponse() {
+                public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
+
+                    cityField.setText(weather_city);
+                    //updatedField.setText(weather_updatedOn);
+                    //detailsField.setText(weather_description);
+                    //currentTemperatureField.setText(weather_temperature);
+                    //humidity_field.setText("Humidity: "+weather_humidity);
+                    //pressure_field.setText("Pressure: "+weather_pressure);
+                    //weatherIcon.setText(Html.fromHtml(weather_iconText));
+
+                }
+            });
+            asyncTask.execute(s,q); //  asyncTask.execute("Latitude", "Longitude")
+
+
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 
 
-
-
-   /* void getLocation() {
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(Main.class.getSimpleName(), "Can't connect to Google Play Services!");
+    }
+    //Check if loc is enabled
+    void getLocation() {
         try {
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, this);
@@ -99,21 +188,17 @@ public class Main extends AppCompatActivity {
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        //city.setText("Latitude: " + location.getLatitude() + "\n Longitude: " + location.getLongitude());
-        lat=location.getLatitude();
-        lon=location.getLongitude();
-        /*try {
-            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-            locationText.setText(locationText.getText() + "\n"+addresses.get(0).getAddressLine(0)+", "+
-                    addresses.get(0).getAddressLine(1)+", "+addresses.get(0).getAddressLine(2));
-        }catch(Exception e)
-        {
-        }*/
-    /*}
+    public void onProviderEnabled(String provider) {
 
-   /* @Override
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+    @Override
     public void onProviderDisabled(String provider) {
         /*Toast toast = Toast.makeText(getApplicationContext(),
                 "Custom toast background color",
@@ -123,17 +208,11 @@ public class Main extends AppCompatActivity {
         toastView.setBackgroundResource(R.drawable.toast_drawable);
         toast.show();*/
 
-        /*Toast.makeText(Main.this, "Please Enable Location and restart the app", Toast.LENGTH_SHORT).show();
+        Toast.makeText(Main.this, "Please Enable Location", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-    }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }*/
 }
+
 

8490f15251106d47cdaac6a8f0f7811fed975952
YS <yogendrasingh890@gmail.com>
2018-01-20 18:32:23 +0530

    added periodic recurring service.

--- app/src/main/java/app/ys/weather_ping21/ForegroundService.java
+++ app/src/main/java/app/ys/weather_ping21/ForegroundService.java
@@ -1,6 +1,5 @@
 package app.ys.weather_ping21;
 
-
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -10,108 +9,115 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.text.Html;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class ForegroundService extends Service {
-    private static final String LOG_TAG = "ForegroundService";
-    SharedPreferences switches;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
+import static com.google.android.gms.internal.zzip.runOnUiThread;
+
+public class ForegroundService extends Service {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    ScheduledFuture beeperHandle;
+    private static final String LOG_TAG = "ForegroundService";
+    LocationManager locationManager;
 
 
     @Override
     public void onCreate()
     {
         super.onCreate();
-        switches = getSharedPreferences("toggle", Context.MODE_PRIVATE);
-    }
 
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent.getAction().equals(Constants.ACTION.STARTFOREGROUND_ACTION)) {
-            //Toast.makeText(this,"Start Service",Toast.LENGTH_SHORT).show();
             Log.i(LOG_TAG, "Received Start Foreground Intent ");
 
-
-            Intent notificationIntent = new Intent(this, Main.class);
-            notificationIntent.setAction(Constants.ACTION.MAIN_ACTION);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
-                    notificationIntent, 0);
-
-            if((switches.getString("Toggle3", null))=="On")
-            {
-            //RemoteViews notificationView = new RemoteViews(this.getPackageName(),R.layout.notification_dark);
-                Bitmap icon = BitmapFactory.decodeResource(getResources(),
-                        R.drawable.logoq);
-
-                Notification notification = new NotificationCompat.Builder(this)
-                        .setContentTitle("WeatherPing")
-                        //.setTicker("ping ping")
-                        .setContentText("wohoooo")
-                        .setSmallIcon(R.drawable.logoq)
-                        .setLargeIcon(
-                                Bitmap.createScaledBitmap(icon, 128, 128, true))
-                        //.setContent(notificationView)
-                        .setOngoing(true).build();
-
-
-
-                startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
-                        notification);
             }
-            else
-            {
-                //RemoteViews notificationView = new RemoteViews(this.getPackageName(),R.layout.notification_dark);
-                Bitmap icon = BitmapFactory.decodeResource(getResources(),
-                        R.drawable.logoq);
-
-                Notification notification = new NotificationCompat.Builder(this)
-                        .setContentTitle("WeatherPing")
-                        .setTicker("ping ping")
-                        .setContentText("wohoooo")
-                        .setSmallIcon(R.drawable.logoq)
-                        .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
-                        //.setContent(notificationView)
-                        .setOngoing(true).build();
-
-
-
-                startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
-                        notification);
-            }
-
-
-
-
-        }
-
 
         else
-            if (intent.getAction().equals(Constants.ACTION.STOPFOREGROUND_ACTION)) {
+        if (intent.getAction().equals(Constants.ACTION.STOPFOREGROUND_ACTION)) {
             Toast.makeText(this,"Stop Service",Toast.LENGTH_SHORT).show();
             Log.i(LOG_TAG, "Received Stop Foreground Intent");
             stopForeground(true);
             stopSelf();
+            /*beeperHandle.cancel(true);
+            scheduler.shutdown();*/
         }
+        takePicsPeriodically(5);
         return START_STICKY;
+
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.i(LOG_TAG, "In onDestroy");
+        beeperHandle.cancel(true);
+        scheduler.shutdown();
     }
-
     @Override
     public IBinder onBind(Intent intent) {
-        // Used only in case of bound services.
+
         return null;
+
     }
 
+    public void takePicsPeriodically(long period) {
+        beeperHandle = scheduler.scheduleAtFixedRate(beeper,period, period,TimeUnit.SECONDS);
+
+    }
+
+    final Runnable beeper = new Runnable() {
+        public void run() {
+            try {
+                sendNotification();
+
+                Log.i("MyTestService", "Service running");
+            }catch (Exception e) {
+                Log.e("TAG","error in executing: It will no longer be run!: "+e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    };
+
+    public void sendNotification() {
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(),
+                R.drawable.logoq);
+
+        //RemoteViews notificationView = new RemoteViews(this.getPackageName(),R.layout.notification_dark);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("WeatherPing")
+                .setTicker("ping ping")
+                .setContentText("")
+                .setSmallIcon(R.drawable.logoq)
+                .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
+                //.setContent(notificationView)
+                .setOngoing(true).build();
+        notification.contentIntent=  PendingIntent.getActivity(this, 0,
+                new Intent(this, Main.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE,
+                notification);
+    }
+
+
 }
+
+

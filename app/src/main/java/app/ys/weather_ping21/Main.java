670416bef70fb034a1268a4d02cbe990864cb717
YS <yogendrasingh890@gmail.com>
2018-02-02 00:25:17 +0530

    minor improvements.

--- app/src/main/java/app/ys/weather_ping21/Main.java
+++ app/src/main/java/app/ys/weather_ping21/Main.java
@@ -53,6 +53,7 @@ public class Main extends AppCompatActivity implements LocationListener {
     public ProgressDialog pDialog;
     TextView locationText,tt1,tt2,tt3,tt4,tt5,tt6,tt7,tt8,tt9,tt10;
     ImageView img,setts;
+    String carbon,sulphur,ozone,pm_10,pm_25,nitrogen;
     String airquality,cityname,aqi;
     private SwipeRefreshLayout swipeRefreshLayout;
     TextView cityField,cloud,latt,detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField,windspeed,winddeg,sun,set;
@@ -81,16 +82,16 @@ public class Main extends AppCompatActivity implements LocationListener {
         setts=(ImageView)findViewById(R.id.sett);
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_to_refresh_layout);
         currentTemperatureField = (TextView)findViewById(R.id.textView6);
-        //tt1=(TextView)findViewById(R.id.textView15);
-        tt2=(TextView)findViewById(R.id.textView23);
-        tt3=(TextView)findViewById(R.id.textView23);
+        tt1=(TextView)findViewById(R.id.textView15);
+        tt2=(TextView)findViewById(R.id.textView24);
+        tt3=(TextView)findViewById(R.id.textView25);
         tt4=(TextView)findViewById(R.id.textView23);
-        tt5=(TextView)findViewById(R.id.textView23);
-        tt6=(TextView)findViewById(R.id.textView23);
-        tt7=(TextView)findViewById(R.id.textView23);
-        tt8=(TextView)findViewById(R.id.textView23);
-        tt9=(TextView)findViewById(R.id.textView23);
-        tt10=(TextView)findViewById(R.id.textView23);
+        tt5=(TextView)findViewById(R.id.textView31);
+        tt6=(TextView)findViewById(R.id.textView32);
+        tt7=(TextView)findViewById(R.id.textView35);
+        tt8=(TextView)findViewById(R.id.textView36);
+        tt9=(TextView)findViewById(R.id.textView47);
+        //tt10=(TextView)findViewById(R.id.textView23);
 
 
         //getLocationBtn = (Button)findViewById(R.id.getLocationBtn);
@@ -438,21 +439,32 @@ public class Main extends AppCompatActivity implements LocationListener {
 
             try {
 
+
+
+
                 JSONObject jsonObject = new JSONObject(data);
                 JSONObject main = jsonObject.getJSONObject("data");
-                JSONObject datas = main.getJSONObject("city");
-                JSONObject aqis = main.getJSONObject("iaqi");
-                JSONObject co = main.getJSONObject("co");
-                JSONObject no2 = main.getJSONObject("no2");
-                JSONObject o3 = main.getJSONObject("o3");
-                JSONObject pm10 = main.getJSONObject("pm10");
-                JSONObject pm25 = main.getJSONObject("pm25");
-                JSONObject so2 = main.getJSONObject("so2");
+                JSONObject mains = jsonObject.getJSONObject("data");
+                JSONObject name = mains.getJSONObject("city");
+
+                JSONObject aqis = mains.getJSONObject("iaqi");
+
+                JSONObject co = aqis.getJSONObject("co");
+                JSONObject no2 = aqis.getJSONObject("no2");
+                JSONObject o3 = aqis.getJSONObject("o3");
+                JSONObject pm10 = aqis.getJSONObject("pm10");
+                JSONObject pm25 = aqis.getJSONObject("pm25");
+                JSONObject so2 = aqis.getJSONObject("so2");
 
 
-                cityname = jsonObject.getString("status");
+
                 aqi = main.getString("aqi");
-                pm10a=pm10.getString("v");
+                carbon=co.getString("v");
+                sulphur=so2.getString("v");
+                ozone=o3.getString("v");
+                pm_10=pm10.getString("v");
+                pm_25=pm25.getString("v");
+                nitrogen=no2.getString("v");
 
                 JSONArray jsonarray = main.getJSONArray("attributions");
                 jsonarray.getJSONObject(0);

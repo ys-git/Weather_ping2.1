b38935a727845448b08fc55514b3c6d268b4900f
YS <yogendrasingh890@gmail.com>
2018-02-09 11:51:44 +0530

    tweaked json parsing.

--- app/src/main/java/app/ys/weather_ping21/Main.java
+++ app/src/main/java/app/ys/weather_ping21/Main.java
@@ -856,22 +856,28 @@ public class Main extends AppCompatActivity implements LocationListener {
                     if(aqis.has("co")){
                         JSONObject co = aqis.getJSONObject("co");
                         carbon=co.getString("v");
+                        tt7.setText("CO :   "+carbon);
 
                     } else {
+                        tt7.setText("CO : No data");
 
                     }
                     if(aqis.has("no2")){
                         JSONObject no2 = aqis.getJSONObject("no2");
                         nitrogen=no2.getString("v");
+                        tt5.setText("NO₂ :   "+nitrogen);
 
                     } else {
+                        tt5.setText("NO₂ : No data");
 
                     }
                     if(aqis.has("o3")){
                         JSONObject o3 = aqis.getJSONObject("o3");
                         ozone=o3.getString("v");
+                        tt4.setText("O₃ :   "+ozone);
 
                     } else {
+                        tt4.setText("O₃ : No data");
 
                     }
                     if(aqis.has("pm10")){
@@ -887,15 +893,19 @@ public class Main extends AppCompatActivity implements LocationListener {
                     if(aqis.has("pm25")){
                         JSONObject pm25 = aqis.getJSONObject("pm25");
                         pm_25=pm25.getString("v");
+                        tt2.setText("PM₂.₅ :   "+pm_25);
 
                     } else {
+                        tt2.setText("PM₂.₅ : No data");
 
                     }
                     if(aqis.has("so2")){
                         JSONObject so2 = aqis.getJSONObject("so2");
                         sulphur=so2.getString("v");
+                        tt6.setText("SO₂ :   "+sulphur);
 
                     } else {
+                        tt6.setText("SO₂ : No data");
 
                     }
 
@@ -912,12 +922,12 @@ public class Main extends AppCompatActivity implements LocationListener {
                     //String name = jsonobject.getString("name");
                     //String url = jsonobject.getString("url");
 
-                tt2.setText("PM₂.₅ :   "+pm_25);
 
-                tt4.setText("O₃ :   "+ozone);
-                tt5.setText("NO₂ :   "+nitrogen);
-                tt6.setText("SO₂ :   "+sulphur);
-                tt7.setText("CO :   "+carbon);
+
+
+
+
+
                 tt10.setText(cityname);
                 tt10.setTypeface(tf4);
                 tt11.setText("Measured by: "+board_name);

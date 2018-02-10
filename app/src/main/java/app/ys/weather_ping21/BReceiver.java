b406987a8fff7cd0e1d8595f45caaa0265c6cd4a
YS <yogendrasingh890@gmail.com>
2018-02-10 21:40:28 +0530

    Error control within json parsing.Major Improvements. Added Visibility Field.

---
+++ app/src/main/java/app/ys/weather_ping21/BReceiver.java
@@ -0,0 +1,22 @@
+package app.ys.weather_ping21;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BReceiver extends BroadcastReceiver {
+    String TAG = "MyReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            //boot has completed, now time to start our background service.
+            Log.wtf(TAG, "Device booted up!  Inside Receiver");
+            Intent service = new Intent(context, ForegroundService.class);
+            service.setAction(Constants.ACTION.STARTFOREGROUND_ACTION);
+            context.startService(service);
+            Log.wtf(TAG, "Intent Created");
+        }
+    }
+}

61e5cc1c61d2b375dfd94dfa808ca617fc7f170e
YS <yogendrasingh890@gmail.com>
2018-02-11 15:45:31 +0530

    various changes.

--- app/src/main/java/app/ys/weather_ping21/BReceiver.java
+++ app/src/main/java/app/ys/weather_ping21/BReceiver.java
@@ -3,6 +3,7 @@ package app.ys.weather_ping21;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 public class BReceiver extends BroadcastReceiver {
@@ -10,13 +11,17 @@ public class BReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            //boot has completed, now time to start our background service.
-            Log.wtf(TAG, "Device booted up!  Inside Receiver");
-            Intent service = new Intent(context, ForegroundService.class);
-            service.setAction(Constants.ACTION.STARTFOREGROUND_ACTION);
-            context.startService(service);
-            Log.wtf(TAG, "Intent Created");
+
+        SharedPreferences switches = context.getSharedPreferences("toggle", Context.MODE_PRIVATE);
+        if ((switches.getInt("Toggle3", -1)) == 1){
+            if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+                //boot has completed, now time to start our background service.
+                Log.wtf(TAG, "Device booted up!  Inside Receiver");
+                Intent service = new Intent(context, ForegroundService.class);
+                service.setAction(Constants.ACTION.STARTFOREGROUND_ACTION);
+                context.startService(service);
+                Log.wtf(TAG, "Intent Created");
+            }
         }
     }
 }
